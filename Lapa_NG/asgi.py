df5341740e48887fc64c7b11f12d733300cc7e82
spudac <iamaspuduck@gmail.com>
2025-10-07 11:47:02 +0700

    Initial Django Initiation

---
+++ Lapa_NG/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Lapa_NG project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lapa_NG.settings')
+
+application = get_asgi_application()
