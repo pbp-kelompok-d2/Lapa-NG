361571599f9d1a4a670a359061a2c746b550e64a
spudac <iamaspuduck@gmail.com>
2025-10-26 18:00:29 +0700

    undoing previous fix attempt

--- Lapa_NG/wsgi.py
+++ Lapa_NG/wsgi.py
@@ -10,16 +10,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lapa_NG.settings')
 
 application = get_wsgi_application()
-
-if os.environ.get("RUN_COLLECTSTATIC_ON_STARTUP") == "1":
-    try:
-        call_command("collectstatic", "--noinput", verbosity=0)
-    except Exception:
-        # swallow and log, so a failure doesn't break startup
-        import logging
-        logging.exception("collectstatic at startup failed")
