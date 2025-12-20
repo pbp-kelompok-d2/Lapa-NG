13384d979fb980705d0aff3c082b57fb7fc683ec
Abhirama <pria.abhirama@ui.ac.id>
2025-12-21 01:49:11 +0700

    Perbaikan fitur booking

--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from booking.views import booking_page, add_to_cart, checkout_page, edit_booking, remove_from_cart, checkout_confirm, view_cart, booking_list, clear_booking
-from booking.views import api_create_booking, api_list_bookings, api_delete_booking, api_update_booking
+from booking.views import api_create_booking, api_list_bookings, api_delete_booking, api_update_booking, api_update_booking_status
 
 app_name = 'booking'
 
@@ -18,4 +18,5 @@ urlpatterns = [
     path("api/list/<int:user_id>/", api_list_bookings, name="api_list_bookings"),
     path("api/delete/<int:booking_id>/", api_delete_booking, name="api_delete_booking"),
     path("api/update/<int:booking_id>/", api_update_booking, name="api_update_booking"),
+    path("api/update-status/<int:booking_id>/", api_update_booking_status, name="api_update_"),
 ]
