6d06c961f7f8b14b72ee6b123915ed1343cf4574
spudac <iamaspuduck@gmail.com>
2025-12-21 18:12:17 +0700

    bugfixing session problems in pws

--- Lapa_NG/settings.py
+++ Lapa_NG/settings.py
@@ -194,7 +194,11 @@ LOGGING = {
         },
     },
 }
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "https://abdurrahman-ammar-lapang.pbp.cs.ui.ac.id",
+    "localhost:8000",
+]
+
 CORS_ALLOW_CREDENTIALS = True
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
